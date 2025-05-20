e2e5d2d8edf4800cdd268027a7540e383e2907b7
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2025-05-20 11:22:20 +0900

    🪭 Fluid tooltip fix for create 6

--- src/main/java/com/petrolpark/destroy/mixin/compat/jei/CreateRecipeCategoryMixin.java
+++ src/main/java/com/petrolpark/destroy/mixin/compat/jei/CreateRecipeCategoryMixin.java
@@ -7,11 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import com.simibubi.create.foundation.utility.CreateLang;
-import mezz.jei.api.gui.ingredient.IRecipeSlotRichTooltipCallback;
 import mezz.jei.api.gui.ingredient.IRecipeSlotView;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,39 +19,35 @@ import com.petrolpark.destroy.chemistry.legacy.ClientMixture;
 import com.petrolpark.destroy.client.DestroyLang;
 import com.petrolpark.destroy.compat.jei.DestroyJEI;
 import com.petrolpark.destroy.config.DestroyAllConfigs;
-import com.simibubi.create.AllFluids;
 import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
 import com.simibubi.create.compat.jei.category.CreateRecipeCategory.Info;
-import com.simibubi.create.content.fluids.potion.PotionFluidHandler;
 import com.simibubi.create.content.fluids.transfer.EmptyingRecipe;
 import com.simibubi.create.content.fluids.transfer.FillingRecipe;
 import com.simibubi.create.content.kinetics.mixer.CompactingRecipe;
 import com.simibubi.create.content.kinetics.mixer.MixingRecipe;
 import com.simibubi.create.content.processing.sequenced.SequencedAssemblyRecipe;
-import net.createmod.catnip.lang.Lang;
 
-import mezz.jei.api.forge.ForgeTypes;
 import mezz.jei.api.recipe.RecipeIngredientRole;
-import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = CreateRecipeCategory.class, remap = false)
 public abstract class CreateRecipeCategoryMixin<T extends Recipe<?>> {
 
-    private static final DecimalFormat df = new DecimalFormat();
+    @Unique
+    private static final DecimalFormat destroy$df = new DecimalFormat();
     static {
-        df.setMinimumFractionDigits(3);
-        df.setMaximumFractionDigits(3);
+        destroy$df.setMinimumFractionDigits(3);
+        destroy$df.setMaximumFractionDigits(3);
     };
 
     /**
      * A map of the IDs of Create Categories to the classes of Recipe those Categories describe.
      */
+    @Unique
     private static final Map<String, Class<? extends Recipe<?>>> CATEGORIES_AND_CLASSES = new HashMap<>();
 
     static {
@@ -82,14 +76,9 @@ public abstract class CreateRecipeCategoryMixin<T extends Recipe<?>> {
             DestroyJEI.MIXTURE_APPLICABLE_RECIPE_TYPES.put(info.recipeType(), CATEGORIES_AND_CLASSES.get(recipeTypeId));
         };
     };
-    
-    /**
-     * Copied from the {@link com.simibubi.create.compat.jei.category.CreateRecipeCategory#addFluidTooltip Create source code} because I can't be bothered to deal with Injection.
-     * Modifies the tooltip for Fluid Stacks which are {@link com.petrolpark.destroy.chemistry.legacy.LegacyMixture Mixtures}.
-     */
 
     @Inject(
-            method = "Lcom/simibubi/create/compat/jei/category/CreateRecipeCategory;addPotionTooltip(Lmezz/jei/api/gui/ingredient/IRecipeSlotView;Ljava/util/List;)V",
+            method = "addPotionTooltip(Lmezz/jei/api/gui/ingredient/IRecipeSlotView;Ljava/util/List;)V",
             at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/Optional;get()Ljava/lang/Object;"),
             remap = false,
             locals = LocalCapture.CAPTURE_FAILHARD)
@@ -108,13 +97,18 @@ public abstract class CreateRecipeCategoryMixin<T extends Recipe<?>> {
                 if (!mixtureTag.isEmpty()) {
                     ClientMixture mixture = ClientMixture.readNBT(ClientMixture::new, mixtureTag);
                     name = mixture.getName();
-                    mixtureTooltip = mixture.getContentsTooltip(iupac, false, false, ((FluidStack) displayed.get()).getAmount(), df);
+                    mixtureTooltip = mixture.getContentsTooltip(iupac, false, false, ((FluidStack) displayed.get()).getAmount(), destroy$df);
                 } else {
                     mixtureTooltip = List.of(DestroyLang.translate("mixture.empty").component());
                 };
             };
-            tooltip.add(name);
-            tooltip.addAll(mixtureTooltip);
+
+            if (tooltip.isEmpty()) {
+                tooltip.add(0, name);
+            } else {
+                tooltip.set(0, name);
+            };
+            tooltip.addAll(1, mixtureTooltip);
         };
     };
 };
