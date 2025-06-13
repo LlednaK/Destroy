826c88f32dfc92a0916179984bf3e5a85e300146
matwi <matwic103@gmail.com>
2025-06-13 15:06:28 +0200

    New functions :)

--- src/main/java/com/petrolpark/destroy/compat/computercraft/apis/ChemistryApi.java
+++ src/main/java/com/petrolpark/destroy/compat/computercraft/apis/ChemistryApi.java
@@ -1,6 +1,10 @@
 package com.petrolpark.destroy.compat.computercraft.apis;
 
+import com.petrolpark.destroy.chemistry.legacy.LegacyReaction;
 import com.petrolpark.destroy.chemistry.legacy.LegacySpecies;
+import com.petrolpark.destroy.chemistry.legacy.genericreaction.GenericReaction;
+import com.petrolpark.destroy.chemistry.legacy.index.DestroyReactions;
+import com.simibubi.create.Create;
 import com.simibubi.create.compat.computercraft.implementation.CreateLuaTable;
 import dan200.computercraft.api.lua.ILuaAPI;
 import dan200.computercraft.api.lua.LuaFunction;
@@ -8,7 +12,7 @@ import org.jetbrains.annotations.Nullable;
 
 public class ChemistryApi implements ILuaAPI {
 
-    @LuaFunction
+    @LuaFunction(mainThread = true)
     public final CreateLuaTable getMoleculeInfos(String id) {
         LegacySpecies molecule = LegacySpecies.getMolecule(id);
         CreateLuaTable infos = new CreateLuaTable();
@@ -20,6 +24,44 @@ public class ChemistryApi implements ILuaAPI {
         infos.putString("FROWNS", molecule.getFROWNSCode());
 
         return infos;
+    }
+
+    @LuaFunction(mainThread = true)
+    public final CreateLuaTable getReactionsFor(String id) {
+        CreateLuaTable resultTable = new CreateLuaTable();
+        LegacySpecies molecule = LegacySpecies.getMolecule(id);
+
+        LegacyReaction.REACTIONS.forEach((rId, reaction) -> {
+            if ( reaction.getProducts().contains(molecule)) {
+                resultTable.putDouble(rId, reaction.getProductMolarRatio(molecule));
+            }
+        });
+
+        return resultTable;
+    }
+
+    @LuaFunction(mainThread = true)
+    public final CreateLuaTable getReactantsFor(String reactionId) {
+        CreateLuaTable resultTable = new CreateLuaTable();
+        LegacyReaction reaction = LegacyReaction.REACTIONS.get(reactionId);
+
+        reaction.getReactants().forEach((reactant) -> {
+            resultTable.putDouble(reactant.getFullID(), reaction.getReactantMolarRatio(reactant));
+        });
+
+        return resultTable;
+    }
+
+    @LuaFunction(mainThread = true)
+    public final CreateLuaTable getProductsFor(String reactionId) {
+        CreateLuaTable resultTable = new CreateLuaTable();
+        LegacyReaction reaction = LegacyReaction.REACTIONS.get(reactionId);
+
+        reaction.getProducts().forEach((product) -> {
+            resultTable.putDouble(product.getFullID(), reaction.getReactantMolarRatio(product));
+        });
+
+        return resultTable;
     }
 
     @Override
